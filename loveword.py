29220b5950ac5622ff6211b3b4e895727cf239f7
guzhongyue666 <74693796+qzpzd@users.noreply.github.com>
2023-05-18 15:01:29 +0800

    Update loveword.py

--- loveword.py
+++ loveword.py
@@ -32,7 +32,7 @@ def getIP():
 def getlovewords():
     #getIP()
     headers={
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46'
     }
     # 获取情话
     texts=[]
